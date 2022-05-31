0fb8008eba02568b5d174aa822fbefc7fc3857eb
pangchun <1007920179@qq.com>
2022-05-31 10:50:57 +0800

    antd自定义表单校验

--- src/bpmn/panel/base/ElementBaseInfo.tsx
+++ src/bpmn/panel/base/ElementBaseInfo.tsx
@@ -1,7 +1,8 @@
-import React, { useEffect } from 'react';
-import { Form, Input, Switch, Typography } from 'antd';
+import React, { useEffect, useState } from 'react';
+import { Form, Input, message, Switch, Typography } from 'antd';
 import { Collapse } from 'antd';
 import { PushpinTwoTone } from '@ant-design/icons';
+import { useInterval } from 'ahooks';
 
 const { Panel } = Collapse;
 
@@ -49,8 +50,11 @@ export default function ElementBaseInfo(props: IProps) {
       // id不能相同, todo 结合antd的自定义校验规则，显示错误提示
       try {
         window.bpmnInstance.elementRegistry._validateId(value);
-      } catch (e) {
+      } catch (e: any) {
         console.log('id重复', e);
+        message
+          .error('已添加编号为' + value + '的元素, 请重新设置编号')
+          .then((r) => {});
         return;
       }
       window.bpmnInstance.modeling.updateProperties(
@@ -95,6 +99,19 @@ export default function ElementBaseInfo(props: IProps) {
     }
   }
 
+  function validateId(value: string) {
+    console.log(value);
+    if (value.includes(' ')) {
+      return {
+        status: false,
+        message: '编号中不能包含空格',
+      };
+    }
+    return {
+      status: true,
+    };
+  }
+
   return (
     <>
       <Collapse
@@ -117,7 +134,17 @@ export default function ElementBaseInfo(props: IProps) {
             <Form.Item
               label="编号"
               name="id"
-              rules={[{ required: true, message: '编号不能为空哦!' }]}
+              rules={[
+                { required: true, message: '编号不能为空哦!' },
+                {
+                  validator: (_, value) => {
+                    const validateId1 = validateId(value);
+                    return validateId1.status
+                      ? Promise.resolve()
+                      : Promise.reject(new Error(validateId1.message));
+                  },
+                },
+              ]}
             >
               <Input
                 readOnly={businessObject?.$type === 'bpmn:Process'}
