93fd5c1cf48bb4bf3c386e3982738a8b2f140af7
pangchun <1007920179@qq.com>
2022-12-04 09:50:53 +0800

    修改背景版颜色

--- src/components/ProcessDesigner/ProcessDesigner.tsx
+++ src/components/ProcessDesigner/ProcessDesigner.tsx
@@ -436,9 +436,9 @@ export default function ProcessDesigner() {
           <div
             id="canvas"
             style={{
-              backgroundColor: '#cccccc',
+              backgroundColor: '#d9d9d9',
               backgroundImage:
-                'linear-gradient(rgba(24,144,255, .5) 1px, transparent 0), linear-gradient(90deg,rgba(24,144,255, .5) 1px, transparent 0)',
+                'linear-gradient(rgba(230, 153, 255, .5) 1px, transparent 0), linear-gradient(90deg,rgba(230, 153, 255, .5) 1px, transparent 0)',
               backgroundSize: '20px 20px',
             }}
           />
