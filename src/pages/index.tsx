4bc27f913e0972bd5a8ed93a06853eb74450801a
pangchun <1007920179@qq.com>
2022-12-24 00:20:58 +0800

    设置窗口宽度限制

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,38 @@
 import ProcessDesigner from '@/components/ProcessDesigner/ProcessDesigner';
 import { Provider } from 'react-redux';
 import { store } from '@/redux/store/store';
-import { Button } from 'antd';
+import { message, Skeleton } from 'antd';
 
 // 引入样式
 import './index.less';
+import { useEffect, useState } from 'react';
 
 export default function IndexPage() {
+  const [visible, setVisible] = useState<boolean>(
+    document.documentElement.clientWidth > 1080,
+  );
+
+  useEffect(() => {
+    watchClientWidth();
+    window.addEventListener('resize', watchClientWidth);
+  }, []);
+
+  function watchClientWidth() {
+    let clientWidth = document.documentElement.clientWidth;
+    if (clientWidth < 1080) {
+      setVisible(false);
+      message.warning('请保证您的窗口宽带大于1080').then(() => {});
+    } else {
+      setVisible(true);
+    }
+  }
+
   return (
     <Provider store={store}>
       <div>
-        <ProcessDesigner />
+        <Skeleton loading={!visible} active>
+          <ProcessDesigner />
+        </Skeleton>
       </div>
     </Provider>
   );
